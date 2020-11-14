3fbdd427e92fde68605f82284631d6ebc6c4d993
Brent Groves <brent.groves@gmail.com>
2020-11-14 10:22:56 -0500

    Added/Changed source code

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const mariadb = require("mariadb");
-const mqtt = require('mqtt');
-const common = require('@bgroves/common');
-
-
-
+const mqtt = require("mqtt");
+const common = require("@bgroves/common");
 
 const {
   MQTT_SERVER,
@@ -12,7 +9,7 @@ const {
   MYSQL_PORT,
   MYSQL_USERNAME,
   MYSQL_PASSWORD,
-  MYSQL_DATABASE
+  MYSQL_DATABASE,
 } = process.env;
 
 /*
@@ -32,12 +29,14 @@ const connectionString = {
   port: MYSQL_PORT,
   user: MYSQL_USERNAME,
   password: MYSQL_PASSWORD,
-  database: MYSQL_DATABASE
-}
+  database: MYSQL_DATABASE,
+};
 
-common.log(`user: ${MYSQL_USERNAME},password: ${MYSQL_PASSWORD}, database: ${MYSQL_DATABASE}, MYSQL_HOSTNAME: ${MYSQL_HOSTNAME}, MYSQL_PORT: ${MYSQL_PORT}`);
+common.log(
+  `user: ${MYSQL_USERNAME},password: ${MYSQL_PASSWORD}, database: ${MYSQL_DATABASE}, MYSQL_HOSTNAME: ${MYSQL_HOSTNAME}, MYSQL_PORT: ${MYSQL_PORT}`
+);
 
-const pool = mariadb.createPool( connectionString);
+const pool = mariadb.createPool(connectionString);
 
 /*
 set @CNC_Approved_Workcenter_Key = 1;
@@ -54,13 +53,24 @@ SELECT @Tool_Life_Key,@Return_Value;
 
 */
 
-async function InsToolLifeHistory(CNC_Approved_Workcenter_Key,Set_No,Block_No,Run_Quantity,Run_Date) {
+async function InsToolLifeHistory(
+  CNC_Approved_Workcenter_Key,
+  Set_No,
+  Block_No,
+  Run_Quantity,
+  Run_Date
+) {
   let conn;
   try {
-    conn = await pool.getConnection();      
-    console.log(`In InsToolLifeHistory with params CNC_Approved_Workcenter_Key=${CNC_Approved_Workcenter_Key},Set_No=${Set_No},Block_No=${Block_No},Run_Quantity=${Run_Quantity},Run_Date=${Run_Date}`)
+    conn = await pool.getConnection();
+    console.log(
+      `In InsToolLifeHistory with params CNC_Approved_Workcenter_Key=${CNC_Approved_Workcenter_Key},Set_No=${Set_No},Block_No=${Block_No},Run_Quantity=${Run_Quantity},Run_Date=${Run_Date}`
+    );
     // const someRows = await conn.query('call UpdateCNCToolOpPartLifeCurrentValue(1,1,1,6,"2020-08-25 10:17:55",@ReturnValue); select @Tool_Life_Key as pTool_Life_Key, @ReturnValue as pReturnValue');
-    const someRows = await conn.query('call InsToolLifeHistory(?,?,?,?,?,@Tool_Life_Key,@Return_Value); select @Tool_Life_Key as pTool_Life_Key,@Return_Value as pReturn_Value',[CNC_Approved_Workcenter_Key,Set_No,Block_No,Run_Quantity,Run_Date]);
+    const someRows = await conn.query(
+      "call InsToolLifeHistory(?,?,?,?,?,@Tool_Life_Key,@Return_Value); select @Tool_Life_Key as pTool_Life_Key,@Return_Value as pReturn_Value",
+      [CNC_Approved_Workcenter_Key, Set_No, Block_No, Run_Quantity, Run_Date]
+    );
     let returnValue = someRows[1][0].pReturn_Value;
     let toolLifeKey = someRows[1][0].pTool_Life_Key;
     console.log(`InsToolLifeHistory.returnValue=${returnValue}`);
@@ -72,12 +82,22 @@ async function InsToolLifeHistory(CNC_Approved_Workcenter_Key,Set_No,Block_No,Ru
     if (conn) conn.release(); //release to pool
   }
 }
-async function InsToolLifeHistoryV2(CNC_Approved_Workcenter_Key,Tool_Var,Run_Quantity,Run_Date) {
+async function InsToolLifeHistoryV2(
+  CNC_Approved_Workcenter_Key,
+  Tool_Var,
+  Run_Quantity,
+  Run_Date
+) {
   let conn;
   try {
-    conn = await pool.getConnection();      
-    console.log(`In InsToolLifeHistoryV2 with params CNC_Approved_Workcenter_Key=${CNC_Approved_Workcenter_Key},Tool_Var=${Tool_Var},Run_Quantity=${Run_Quantity},Run_Date=${Run_Date}`)
-    const someRows = await conn.query('call InsToolLifeHistoryV2(?,?,?,?,@Tool_Life_Key,@Return_Value); select @Tool_Life_Key as pTool_Life_Key,@Return_Value as pReturn_Value',[CNC_Approved_Workcenter_Key,Tool_Var,Run_Quantity,Run_Date]);
+    conn = await pool.getConnection();
+    console.log(
+      `In InsToolLifeHistoryV2 with params CNC_Approved_Workcenter_Key=${CNC_Approved_Workcenter_Key},Tool_Var=${Tool_Var},Run_Quantity=${Run_Quantity},Run_Date=${Run_Date}`
+    );
+    const someRows = await conn.query(
+      "call InsToolLifeHistoryV2(?,?,?,?,@Tool_Life_Key,@Return_Value); select @Tool_Life_Key as pTool_Life_Key,@Return_Value as pReturn_Value",
+      [CNC_Approved_Workcenter_Key, Tool_Var, Run_Quantity, Run_Date]
+    );
     let returnValue = someRows[1][0].pReturn_Value;
     let toolLifeKey = someRows[1][0].pTool_Life_Key;
     console.log(`InsToolLifeHistoryV2.returnValue=${returnValue}`);
@@ -89,14 +109,33 @@ async function InsToolLifeHistoryV2(CNC_Approved_Workcenter_Key,Tool_Var,Run_Qua
     if (conn) conn.release(); //release to pool
   }
 }
-async function UpdateCNCToolOpPartLife(CNC_Approved_Workcenter_Key,Set_No,Block_No,Current_Value,Running_Total,Last_Update) {
+async function UpdateCNCToolOpPartLife(
+  CNC_Approved_Workcenter_Key,
+  Set_No,
+  Block_No,
+  Current_Value,
+  Running_Total,
+  Last_Update
+) {
   let conn;
   try {
-    conn = await pool.getConnection();      
-    console.log(`In UpdateCNCToolOpPartLife with params CNC_Approved_Workcenter_Key=${CNC_Approved_Workcenter_Key},Set_No=${Set_No},Block_No=${Block_No},Current_Value=${Current_Value},Running_Total=${Running_Total},Last_Update=${Last_Update}`)
+    conn = await pool.getConnection();
+    console.log(
+      `In UpdateCNCToolOpPartLife with params CNC_Approved_Workcenter_Key=${CNC_Approved_Workcenter_Key},Set_No=${Set_No},Block_No=${Block_No},Current_Value=${Current_Value},Running_Total=${Running_Total},Last_Update=${Last_Update}`
+    );
     // const someRows = await conn.query('call UpdateCNCToolOpPartLifeCurrentValue(1,1,1,6,"2020-08-25 10:17:55",@ReturnValue); select @ReturnValue as pReturnValue');
-    const someRows = await conn.query('call UpdateCNCToolOpPartLife(?,?,?,?,?,?,@ReturnValue); select @ReturnValue as pReturnValue',[CNC_Approved_Workcenter_Key,Set_No,Block_No,Current_Value,Running_Total,Last_Update]);
-    let returnValue = someRows[1][0].pReturnValue
+    const someRows = await conn.query(
+      "call UpdateCNCToolOpPartLife(?,?,?,?,?,?,@ReturnValue); select @ReturnValue as pReturnValue",
+      [
+        CNC_Approved_Workcenter_Key,
+        Set_No,
+        Block_No,
+        Current_Value,
+        Running_Total,
+        Last_Update,
+      ]
+    );
+    let returnValue = someRows[1][0].pReturnValue;
     console.log(`UpdateCNCToolOpPartLife.returnValue=${returnValue}`);
   } catch (err) {
     // handle the error
@@ -105,6 +144,40 @@ async function UpdateCNCToolOpPartLife(CNC_Approved_Workcenter_Key,Set_No,Block_
     if (conn) conn.release(); //release to pool
   }
 }
+async function UpdateCNCToolOpPartLifeV2(
+  CNC_Approved_Workcenter_Key,
+  Tool_Var,
+  Current_Value,
+  Running_Total,
+  Last_Update
+) {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    console.log(
+      `In UpdateCNCToolOpPartLifeV2 with params CNC_Approved_Workcenter_Key=${CNC_Approved_Workcenter_Key},Tool_Var=${Tool_Var},Current_Value=${Current_Value},Running_Total=${Running_Total},Last_Update=${Last_Update}`
+    );
+    // const someRows = await conn.query('call UpdateCNCToolOpPartLifeCurrentValue(1,1,1,6,"2020-08-25 10:17:55",@ReturnValue); select @ReturnValue as pReturnValue');
+    const someRows = await conn.query(
+      "call UpdateCNCToolOpPartLifeV2(?,?,?,?,?,@ReturnValue); select @ReturnValue as pReturnValue",
+      [
+        CNC_Approved_Workcenter_Key,
+        Tool_Var,
+        Current_Value,
+        Running_Total,
+        Last_Update,
+      ]
+    );
+    let returnValue = someRows[1][0].pReturnValue;
+    console.log(`UpdateCNCToolOpPartLifeV2.returnValue=${returnValue}`);
+  } catch (err) {
+    // handle the error
+    console.log(`Error =>${err}`);
+  } finally {
+    if (conn) conn.release(); //release to pool
+  }
+}
+
 /*
 CREATE PROCEDURE InsAssemblyMachiningHistory
 (
@@ -117,16 +190,32 @@ CREATE PROCEDURE InsAssemblyMachiningHistory
 	OUT pReturnValue INT 
 )
 */
-async function InsAssemblyMachiningHistory(CNC_Approved_Workcenter_Key,Pallet_No,Tool_Var,Start_Time,End_Time) {
+async function InsAssemblyMachiningHistory(
+  CNC_Approved_Workcenter_Key,
+  Pallet_No,
+  Tool_Var,
+  Current_Value,
+  Running_Total,
+  Start_Time,
+  End_Time
+) {
   let conn;
   try {
-    conn = await pool.getConnection();      
-    console.log(`In InsAssemblyMachiningHistory with params CNC_Approved_Workcenter_Key=${CNC_Approved_Workcenter_Key},Pallet_No=${Pallet_No},Tool_Var=${Tool_Var},Start_Time=${Start_Time},End_Time=${End_Time}`);    
-    const someRows = await conn.query('call InsAssemblyMachiningHistory(?,?,?,?,?,@Assembly_Machining_History_Key,@Return_Value); select @Assembly_Machining_History_Key as pAssembly_Machining_History_Key,@Return_Value as pReturn_Value',[CNC_Approved_Workcenter_Key,Pallet_No,Tool_Var,Start_Time,End_Time]);
+    conn = await pool.getConnection();
+    console.log(
+      `In InsAssemblyMachiningHistory with params CNC_Approved_Workcenter_Key=${CNC_Approved_Workcenter_Key},Pallet_No=${Pallet_No},Tool_Var=${Tool_Var},Current_Values=${Current_Value},Running_Total=${Running_Total},Start_Time=${Start_Time},End_Time=${End_Time}`
+    );
+    const someRows = await conn.query(
+      "call InsAssemblyMachiningHistory(?,?,?,?,?,?,?,@Assembly_Machining_History_Key,@Return_Value); select @Assembly_Machining_History_Key as pAssembly_Machining_History_Key,@Return_Value as pReturn_Value",
+      [CNC_Approved_Workcenter_Key, Pallet_No, Tool_Var, Current_Value,Running_Total, Start_Time, End_Time]
+    );
     let returnValue = someRows[1][0].pReturn_Value;
-    let Assembly_Machining_History_Key = someRows[1][0].pAssembly_Machining_History_Key;
+    let Assembly_Machining_History_Key =
+      someRows[1][0].pAssembly_Machining_History_Key;
     console.log(`InsAssemblyMachiningHistory.returnValue=${returnValue}`);
-    console.log(`InsAssemblyMachiningHistory.Assembly_Machining_History_Key=${Assembly_Machining_History_Key}`);
+    console.log(
+      `InsAssemblyMachiningHistory.Assembly_Machining_History_Key=${Assembly_Machining_History_Key}`
+    );
   } catch (err) {
     // handle the error
     console.log(`Error =>${err}`);
@@ -135,30 +224,36 @@ async function InsAssemblyMachiningHistory(CNC_Approved_Workcenter_Key,Pallet_No
   }
 }
 
-
 function main() {
   common.log(`MQTT_SERVER=${MQTT_SERVER},MQTT_PORT=${MQTT_PORT}`);
   const mqttClient = mqtt.connect(`mqtt://${MQTT_SERVER}:${MQTT_PORT}`);
 
-  mqttClient.on('connect', function() {
-    mqttClient.subscribe('InsToolLifeHistory', function(err) {
+  mqttClient.on("connect", function () {
+    mqttClient.subscribe("InsToolLifeHistory", function (err) {
       if (!err) {
-        common.log('Tracker13319 has subscribed to: InsToolLifeHistory');
+        common.log("Tracker13319 has subscribed to: InsToolLifeHistory");
       }
-    });                 //UpdateCNCToolOpPartLife
-    mqttClient.subscribe('InsToolLifeHistoryV2', function(err) {
+    }); //UpdateCNCToolOpPartLife
+    mqttClient.subscribe("InsToolLifeHistoryV2", function (err) {
       if (!err) {
-        common.log('Tracker13319 has subscribed to: InsToolLifeHistory');
+        common.log("Tracker13319 has subscribed to: InsToolLifeHistoryV2");
       }
-    });                 //UpdateCNCToolOpPartLife
-    mqttClient.subscribe('UpdateCNCToolOpPartLife', function(err) {
+    }); //UpdateCNCToolOpPartLife
+    mqttClient.subscribe("UpdateCNCToolOpPartLife", function (err) {
       if (!err) {
-        common.log('Tracker13319 has subscribed to: UpdateCNCToolOpPartLife');
+        common.log("Tracker13319 has subscribed to: UpdateCNCToolOpPartLife");
       }
     });
-    mqttClient.subscribe('InsAssemblyMachiningHistory', function(err) {
+    mqttClient.subscribe("UpdateCNCToolOpPartLifeV2", function (err) {
       if (!err) {
-        common.log('Tracker13319 has subscribed to: InsAssemblyMachiningHistory');
+        common.log("Tracker13319 has subscribed to: UpdateCNCToolOpPartLifeV2");
+      }
+    });
+    mqttClient.subscribe("InsAssemblyMachiningHistory", function (err) {
+      if (!err) {
+        common.log(
+          "Tracker13319 has subscribed to: InsAssemblyMachiningHistory"
+        );
       }
     });
   });
@@ -190,34 +285,66 @@ CREATE PROCEDURE InsAssemblyMachiningHistory
   */
 
   // message is a buffer
-  mqttClient.on('message', function(topic, message) {
+  mqttClient.on("message", function (topic, message) {
     const obj = JSON.parse(message.toString()); // payload is a buffer
     common.log(`Tracker13319 => ${message.toString()}`);
-   // let Last_Update = obj.Last_Update.toString();
-   // common.log(`Tracker13319.Last_Update => ${Last_Update}`);
+    // let Last_Update = obj.Last_Update.toString();
+    // common.log(`Tracker13319.Last_Update => ${Last_Update}`);
 
-    switch(topic) {
-      case 'InsToolLifeHistory':
-        InsToolLifeHistory(obj.CNC_Approved_Workcenter_Key,obj.Set_No,obj.Block_No,obj.Run_Quantity,obj.Run_Date);      
+    switch (topic) {
+      case "InsToolLifeHistory":
+        InsToolLifeHistory(
+          obj.CNC_Approved_Workcenter_Key,
+          obj.Set_No,
+          obj.Block_No,
+          obj.Run_Quantity,
+          obj.Run_Date
+        );
         break;
-        case 'InsToolLifeHistoryV2':
-          InsToolLifeHistoryV2(obj.CNC_Approved_Workcenter_Key,
-            obj.Tool_Var,obj.obj.Run_Quantity,obj.Run_Date);      
-          break;
-            //UpdateCNCToolOpPartLife
-      case 'UpdateCNCToolOpPartLife':
-        UpdateCNCToolOpPartLife(obj.CNC_Approved_Workcenter_Key,obj.Set_No,obj.Block_No,obj.Current_Value,obj.Running_Total,obj.Last_Update);      
+      case "InsToolLifeHistoryV2":
+        InsToolLifeHistoryV2(
+          obj.CNC_Approved_Workcenter_Key,
+          obj.Tool_Var,
+          obj.Run_Quantity,
+          obj.Run_Date
+        );
         break;
-        //InsAssemblyMachiningHistory
-        case 'InsAssemblyMachiningHistory':
-          InsAssemblyMachiningHistory(obj.CNC_Approved_Workcenter_Key,obj.Pallet_No,obj.Tool_Var,obj.Start_Time,obj.End_Time);      
-          break;
-        default:
-        common.log(`Tracker13319 => topic not found!`)
-        // code block
+      //UpdateCNCToolOpPartLife
+      case "UpdateCNCToolOpPartLife":
+        UpdateCNCToolOpPartLife(
+          obj.CNC_Approved_Workcenter_Key,
+          obj.Set_No,
+          obj.Block_No,
+          obj.Current_Value,
+          obj.Running_Total,
+          obj.Last_Update
+        );
+        break;
+      case "UpdateCNCToolOpPartLifeV2":
+        UpdateCNCToolOpPartLifeV2(
+          obj.CNC_Approved_Workcenter_Key,
+          obj.Tool_Var,
+          obj.Current_Value,
+          obj.Running_Total,
+          obj.Last_Update
+        );
+        break;
+      //InsAssemblyMachiningHistory
+      case "InsAssemblyMachiningHistory":
+        InsAssemblyMachiningHistory(
+          obj.CNC_Approved_Workcenter_Key,
+          obj.Pallet_No,
+          obj.Tool_Var,
+          obj.Current_Value,
+          obj.Running_Total,
+          obj.Start_Time,
+          obj.End_Time
+        );
+        break;
+      default:
+        common.log(`Tracker13319 => topic not found!`);
+      // code block
     }
-
   });
-  
 }
 main();
